135efe979cc974031ce5becd9204cb9015707c46
faizaslam11 <faizaslam11@gmail.com>
2022-01-16 18:20:53 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorComponent } from './doctor/doctor.component';
+import { HomeComponent } from './home/home.component';
+import { PatientComponent } from './patient/patient.component';
+import { PrescriptionComponent } from './prescription/prescription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'patient', component: PatientComponent},
+  {path: 'doctor', component: DoctorComponent},
+  {path: '', component: HomeComponent},
+  {path:'prescription', component: PrescriptionComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { HomeComponent } from './home/home.component';
+import { PrescriptionComponent } from './prescription/prescription.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    DoctorComponent,
+    HomeComponent,
+    PrescriptionComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
